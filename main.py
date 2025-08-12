4d6016d4b74d9c9499360654fcb11a33311f535a
Adityapathak2081 <adityanarayan.22111@gmail.com>
2025-08-12 21:16:00 +0530

    main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import mysql.connector
 
-# Step 1: Connect to MySQL (change these to your MySQL credentials)
 db = mysql.connector.connect(
-    host="localhost",         # usually 'localhost'
-    user="root",              # your MySQL username
-    password="password"  # your MySQL password
+    host="localhost",      
+    user="root",       
+    password="password"
 )
 
 cursor = db.cursor()
 
-# Step 2: Create database if not exists
 cursor.execute("CREATE DATABASE IF NOT EXISTS weather_app")
 print("✅ Database 'weather_app' created or already exists.")
-
-# Step 3: Use that database
 cursor.execute("USE weather_app")
 
-# Step 4: Create table if not exists
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS weather_data (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -29,10 +24,9 @@ CREATE TABLE IF NOT EXISTS weather_data (
     humidity INT,
     wind_speed FLOAT,
     description VARCHAR(100),
-    date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
-""")
+    date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+    """)
 print("✅ Table 'weather_data' created or already exists.")
 
-# Step 5: Close connection
 db.close()
+
